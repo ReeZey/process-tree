ead7e1187d3b8633990ab4f3de36394e3c87c8d1
ReeZey <reezey@reez.it>
2025-05-07 23:06:03 +0200

    converted project to use tokio threads

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
-use std::{fs::{self, File}, path::PathBuf, process::Command, thread};
+use std::{fs::{self}, os::unix::process::ExitStatusExt, path::PathBuf, process::{ExitStatus, Stdio}};
+use serde::Deserialize;
+use tokio::{fs::File, io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader}, process::Command};
+use tokio_stream::StreamExt;
+use tokio_util::codec::{Framed, FramedRead, LinesCodec};
 
-fn main() {
+#[derive(Deserialize, Debug)]
+struct Config {
+    directory: PathBuf,
+    exec: String,
+    retry: Option<u32>,
+}
+
+#[tokio::main]
+async fn main() {
 
     let processes_path = PathBuf::from("processes");
     let log_path = PathBuf::from("logs");
@@ -30,23 +42,77 @@ fn main() {
             },
             None => {}
         };
+
+        let mut current_file = File::open(&file).await.expect("failed to open file");
+        let mut contents = String::new();
+        current_file.read_to_string(&mut contents).await.expect("failed to read file");
         
+        let config: Config = toml::from_str(contents.as_str()).unwrap();
+
+        let retry = config.retry.unwrap_or(5);
+
         let mut log_file = log_path.join(file.file_name().unwrap());
         log_file.set_extension("log");
 
-        let thread = thread::spawn(move || {
-            let log = File::create(log_file).expect("failed to open log");
+        let thread = tokio::spawn(async move {
+            let mut log = File::create(log_file).await.expect("failed to open log");
 
-            let error_log = log.try_clone().expect("could not clone log");
-            
-            
-            let mut command = Command::new(file)
-                .stdout(log)
-                .stderr(error_log)
+            let mut command = Command::new(config.exec.clone())
+                .current_dir(config.directory)
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
                 .spawn()
                 .expect("Failed to start process");
 
-            command.wait().unwrap();
+            let stdout = command.stdout.take().unwrap();
+            let stderr = command.stderr.take().unwrap();
+
+            let mut stdout_framed = FramedRead::new(stdout, LinesCodec::new());
+            let mut stderr_framed = FramedRead::new(stderr, LinesCodec::new());
+            
+            let exit_status = loop {
+                let abc = tokio::time::timeout(tokio::time::Duration::from_millis(100), command.wait()).await;
+                match abc {
+                    Ok(Ok(status)) => {
+                        println!("Process finished: {:?}", config.exec);
+
+                        break status;
+                    }
+                    Ok(Err(e)) => {
+                        println!("Error waiting for process: {:?}", e);
+
+                        break ExitStatus::from_raw(1);
+                    }
+                    Err(_) => {}
+                }
+
+                println!("Process still running: {:?}", config.exec);
+
+                
+                
+                while let Some(line) = stdout_framed.next().await {
+                    match line {
+                        Ok(text) => {
+                            log.write_all(text.as_bytes()).await.expect("failed to write to log");
+                            log.write_all(b"\n").await.expect("failed to write to log");
+                        },
+                        Err(e) => eprintln!("Error reading: {}", e),
+                    }
+                }
+
+                while let Some(line) = stderr_framed.next().await {
+                    match line {
+                        Ok(text) => {
+                            log.write_all(text.as_bytes()).await.expect("failed to write to log");
+                            log.write_all(b"\n").await.expect("failed to write to log");
+                        },
+                        Err(e) => eprintln!("Error reading: {}", e),
+                    }
+                }
+            };
+
+
+            exit_status
         });
 
         threads.push(thread);
@@ -54,8 +120,11 @@ fn main() {
 
     loop {
         let mut dead_threads = vec![];
-        for (idx, thread) in threads.iter().enumerate() {
+        for (idx, thread) in threads.iter_mut().enumerate() {
             if thread.is_finished() {
+                let status = thread.await.unwrap();
+
+                println!("Thread finished: {:?}", status);
                 dead_threads.push(idx);
             }
         }
